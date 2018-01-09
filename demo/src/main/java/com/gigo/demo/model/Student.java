a339e024e7eb3629b557414aa7a34454314a4b83
Giridhar Gopal <g.gopal@prowareness.nl>
2018-01-09 08:06:52 +0530

    first student model class.

---
+++ demo/src/main/java/com/gigo/demo/model/Student.java
@@ -0,0 +1,12 @@
+package com.gigo.demo.model;
+
+import java.util.UUID;
+
+public class Student {
+	
+	private final UUID id;
+	private final Integer age;
+	private final String firstName;
+	private final String lastName;
+	private final String course;
+}
